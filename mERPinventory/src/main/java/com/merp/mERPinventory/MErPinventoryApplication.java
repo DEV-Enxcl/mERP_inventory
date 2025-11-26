10f1f47d5d8ae6a4a5bdb72a41d9306593355a3c
Adarshenxcl <adarsh.ramesh@enxcl.com>
2025-11-26 16:54:07 +0530

    Inventory service configuration

---
+++ mERPinventory/src/main/java/com/merp/mERPinventory/MErPinventoryApplication.java
@@ -0,0 +1,13 @@
+package com.merp.mERPinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MErPinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MErPinventoryApplication.class, args);
+	}
+
+}
---
+++ mERPinventory/src/test/java/com/merp/mERPinventory/MErPinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merp.mERPinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MErPinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
